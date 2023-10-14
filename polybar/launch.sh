7b17074239e0461ec02b719ee8b213c9e9757946
systeminstall <147773881+systeminstall@users.noreply.github.com>
2023-10-14 21:30:20 +0200

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-# Add this script to your wm startup file.
-
-DIR="$HOME/.config/polybar/forest"
-
 # Terminate already running bar instances
-killall -q polybar
+# If all your bars have ipc enabled, you can use 
+polybar-msg cmd quit
+# Otherwise you can use the nuclear option:
+# killall -q polybar
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar bar1 2>&1 | tee -a /tmp/polybar1.log & disown
 
-# Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+echo "Bars launched..."
